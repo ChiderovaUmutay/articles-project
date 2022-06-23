5bc3565150deda734b6aefc45b900e162065eb0e
Chiderova Umutay <ciderovaumutaj@gmail.com>
2022-06-23 09:02:55 +0600

    добавила в модель Article поле status, создала и применила миграции, реализовала вывод статуса на главной странице

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+STATUS_CHOICES = [('new', 'Новая'), ('moderated', 'Модерированная'), ('rejected', 'Отклоненная')]
+
 
 class Article(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False, verbose_name="Заголовок")
@@ -9,6 +11,7 @@ class Article(models.Model):
     content = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Контент")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, verbose_name="Cтатус", default=STATUS_CHOICES[0][0])
 
     def __str__(self):
         return f"{self.pk}.  {self.title}: {self.author}"
