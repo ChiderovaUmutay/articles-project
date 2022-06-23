30fa14f075a04ad81cb4ce184261d595c3904b80
Chiderova Umutay <ciderovaumutaj@gmail.com>
2022-06-23 10:38:17 +0600

    реализовали страницу со статистикой игры

--- source/game/views.py
+++ source/game/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from game.messages import WIN_MESSAGE
 from game.servce import Game
 
 
@@ -15,10 +15,13 @@ def index_views(request):
             result = error
         else:
             result = game.get_result()
-        return render(request, "game.html", {"result": result, "nums": numbers_str})
+            if result != WIN_MESSAGE:
+                Game.stats_list.append(f"Вы ввели {numbers_str}, результат: {result}")
+        return render(request, "game.html",
+                      {"result": result, "nums": numbers_str, "secret_numbers": game.secret_numbers})
     else:
         return render(request, "game.html")
 
 
 def get_stat(request):
-    pass
+    return render(request, "stat.html", {"stats_list": Game.stats_list})
